3b608a829671b6aece5c615e7338cb0fe19d5a03
jona <jona@jona.com>
2020-05-08 07:49:27 +0300

    solve me first

---
+++ solvemefirst.py
@@ -0,0 +1,10 @@
+def solveMeFirst(a,b):
+    sum =a+b;
+    return sum
+
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+
+res=solveMeFirst(num1,num2)
+
+print(res)
